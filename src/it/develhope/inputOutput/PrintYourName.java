8e236ae0a497fad67fb45c7643a852ceca785e45
ariosto22 <emanuele.digregoli40@gmail.com>
2022-10-18 12:15:58 +0200

    Input and Output 01 Exercise

---
+++ src/it/develhope/inputOutput/PrintYourName.java
@@ -0,0 +1,8 @@
+package it.develhope.inputOutput;
+
+public class PrintYourName {
+    public static void main(String[] args) {
+        String name = "Emanuele";
+        System.out.printf("My name is %s.",name);
+    }
+}
